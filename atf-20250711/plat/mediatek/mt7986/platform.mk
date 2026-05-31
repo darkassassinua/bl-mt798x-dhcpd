7270478f479e332bfdc205f59eae5af85272cb8f
Yuzhii0718 <yuzhii0718@outlook.com>
2026-05-31 20:18:31 +0800

    atf-2025: Revert "atf-2025: add mt7986 trng choice"
    This reverts commit c36baccd2342faa67e8dcb7b39e2f275af906430.

--- atf-20250711/plat/mediatek/mt7986/platform.mk
+++ atf-20250711/plat/mediatek/mt7986/platform.mk
@@ -22,17 +22,10 @@ MULTI_CONSOLE_API	:=	1
 
 RESET_TO_BL2		:=	1
 
-# MT7986 TRNG interface version: 1 for default.
-# set to 2 for new TRNG driver.
-MT7986_TRNG_VERSION ?= 1
-ifeq ($(filter 1 2,$(MT7986_TRNG_VERSION)),)
-$(error MT7986_TRNG_VERSION must be 1 or 2)
-endif
-
 PLAT_INCLUDES		:=	-I$(APSOC_COMMON)				\
 				-I$(APSOC_COMMON)/drivers/uart			\
 				-I$(APSOC_COMMON)/drivers/wdt			\
-				-I$(APSOC_COMMON)/drivers/trng/v$(MT7986_TRNG_VERSION)	\
+				-I$(APSOC_COMMON)/drivers/trng/v1		\
 				-Iinclude/plat/arm/common			\
 				-Iinclude/plat/arm/common/aarch64		\
 				-I$(MTK_PLAT_SOC)/drivers/dram			\
